b54d1bf4a2ef7f2027ed903fe5ca12c85c118936
aparna4scholas <aparna4scholas@gmail.com>
2020-05-08 20:02:35 -0500

    costructor added

--- ship.js
+++ ship.js
@@ -1,6 +1,10 @@
 class Ship {
     costructor(hull,firepower,accuracy,name){
-        this.hull = hull;
-        
+        this.hull = hull; // -20,5
+        this.firepower = firepower;// 
+        this.accuracy = accuracy;// 
+        this.name = name;
+
+
     }
 }
